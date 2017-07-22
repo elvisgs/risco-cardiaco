3dd6860d879a1064dc105997e9ee247f129ea571
Elvis Luciano <elvis.gs@gmail.com>
2017-07-22 00:09:19 -0300

    Atualização: react e react-dom

--- webpack.config.js
+++ webpack.config.js
@@ -67,6 +67,11 @@ var config = {
 
 if (isProduction()) {
   config.plugins = config.plugins.concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
